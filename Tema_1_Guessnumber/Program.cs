944230360cb48c907ab70e43496944e1678341cd
Iza <Iza@DESKTOP-4CJ4JDL>
2018-05-08 22:46:49 +0300

    Guess the number program using Parse,do-while loop, and random generator.

---
+++ Tema_1_Guessnumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_1_Guessnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int RandomValue = rng.Next(1, 100);
+            Console.WriteLine(RandomValue);
+            int nr = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter a number");
+                string str = Console.ReadLine();
+                nr = int.Parse(str);
+
+                if (nr > RandomValue)
+                {
+                    Console.WriteLine("The number is greater than the target number");
+                }
+                else if (nr < RandomValue)
+                {
+                    Console.WriteLine("The number is not greater than the target number");
+                }
+            } while (nr != RandomValue);
+
+            Console.WriteLine("You guessed the number!");
+        }
+    }
+}
