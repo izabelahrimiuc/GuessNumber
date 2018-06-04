22b227c144cedabc4fac1d9e17d9c3381d11ab1e
Iza <Iza@DESKTOP-4CJ4JDL>
2018-06-04 23:57:05 +0300

    Homework 03 -work in progress.

---
+++ Module01Week03/MessageConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using MessageLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageLibrary;
+using static MessageLibrary.Person;
+
+namespace MessageConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User designer = new Person("Rares","Pop",new DateTime(1992,07,18));
+
+            Author author = new Author(5, "Ana", "Rusu", new DateTime(1991,05,1));
+        }
+    }
+}
---
+++ Module01Week03/MessageLibrary/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageLibrary
+{
+    public class User
+    {
+        public string FirstName { get; private set; };
+        public string LastName { get; private set; };
+        public DateTime BirthDate { get; private set; };
+        public string Email { get; private set; };
+
+        public User(string firstName,string lastName, DateTime birthDate, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDate = birthDate;
+            this.Email = email;
+        }
+    }
+
+    public class Author : User
+    {
+        public int Post;
+
+        public Author(int post, string firstName, string lastName, DateTime birthDate, string email)
+            : base(firstName, lastName, birthDate, email)
+        {
+            this.Post = post;
+        }
+    }
+
+    public class Post
+    {
+        public User Author { get; private set; };
+        public DateTime TimeOfPosting { get; private set; };
+        public string Message { get; private set; };
+
+        public Post(User author, DateTime timeofPosting, string message)
+        {
+            this.Author = author;
+            this.TimeOfPosting = timeofPosting;
+            this.Message = message;
+        }
+    }
+
+    public class Board
+    {
+        List<User> userList = new List<User>();
+        List<Post> postList = new List<Post>();
+
+
+    }
+}
