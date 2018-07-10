0547fbeed1fc916f5eb2fe1eef692f8be7b89076
Iza <Iza@DESKTOP-4CJ4JDL>
2018-07-10 22:46:39 +0300

    I started th Web App

---
+++ Module03Week01/Homework01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Homework01.Startup))]
+namespace Homework01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
